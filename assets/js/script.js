f95dba68b025e4da40668f08fdda859363e17267
sarebok <bortonae@hotmail.com>
2023-08-20 12:17:43 -0400

    changing a little of html and css for page order

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,31 +6,25 @@ const a = 1,
 const funcionExpresion = function (a, b, c) {
   return a + b + c;
 };
-console.log(
-  "resultado de la funcion 'funcionExpresion': " + funcionExpresion(a, b, c)
-);
+console.log("resultado de la funcion 'funcionExpresion': " + funcionExpresion(a, b, c));
 
 //ej2: Transforma la siguiente a una funcion arrow de una linea
 let unaLinea = (a, b) => a + b;
 console.log("resultado funcion arrow una linea: " + unaLinea(a, b));
 
 //ej3: Pintar un div
-//modifica la funcion
+//a) modifica la funcion
 const ele = document.getElementById("ele1");
 /* ele.addEventListener("click", () => (ele.style.backgroundColor = "yellow")); */
-//pasar color a funcion pintar
-//verde como color predeterminado
+//b) pasar color a funcion pintar
 ele.style.backgroundColor = "green";
 //funcion para pasar color como parametro
 function pintar(elemento, color = "green") {
-  elemento.addEventListener(
-    "click",
-    () => (elemento.style.backgroundColor = color)
-  );
+  elemento.addEventListener("click", () => (elemento.style.backgroundColor = color));
 }
 pintar(ele, "yellow");
 //ej4:
-//pintar de negro
+//a) pintar de negro
 const blueDiv = document.getElementById("div-azul");
 const redDiv = document.getElementById("div-rojo");
 const greenDiv = document.getElementById("div-verde");
@@ -40,31 +34,28 @@ pintar(redDiv, "black");
 pintar(greenDiv, "black");
 pintar(yellowDiv, "black");
 
-//pintar de otros colores
+//b) pintar de otros colores
 const keyDiv = document.getElementById("key");
-const divsCreados = document.getElementById("divs-creados");
 let colorGlobalGuardado = "white";
 //reconocer las teclas y asociar un color
-function asociarColorATecla(evento, tecla, color) {
-  if (evento === tecla) {
-    colorGlobalGuardado = color;
+
+function asociarColorATecla(evento, tecla, colorDeseado) {
+  if (evento.key === tecla) {
+    colorGlobalGuardado = colorDeseado;
   } else {
     return;
   }
 }
 document.addEventListener("keydown", function (event) {
-  asociarColorATecla(event.key, "a", "pink");
-  asociarColorATecla(event.key, "s", "orange");
-  asociarColorATecla(event.key, "d", "cyan");
-  nuevoPintar(keyDiv, colorGlobalGuardado);
+  asociarColorATecla(event, "a", "pink");
+  asociarColorATecla(event, "s", "orange");
+  asociarColorATecla(event, "d", "cyan");
+  keyDiv.style.backgroundColor = colorGlobalGuardado;
 });
 
-function nuevoPintar(elemento, color) {
-  elemento.style.backgroundColor = color;
-}
-
-//crear divs de colores
-let colorGlobalGuardado2 = "white";
+//c) crear divs de colores
+const divsCreados = document.getElementById("divs-creados");
+let colorGlobalGuardado2 = "white"; //por algun motivo si uso la misma variable global anterior me sobrescribe el div anterior (probar cambiando a colorGlobalGuardao)
 document.addEventListener("keydown", function (ev) {
   if (ev.key === "q") {
     colorGlobalGuardado2 = "purple";
@@ -84,6 +75,6 @@ function crearDiv(color) {
   const divCreado = document.createElement("div");
   divCreado.style.width = "200px";
   divCreado.style.height = "200px";
-  nuevoPintar(divCreado, color);
+  divCreado.style.backgroundColor = color;
   divsCreados.appendChild(divCreado);
 }
